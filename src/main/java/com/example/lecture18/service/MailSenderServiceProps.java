4a88914733bd759fe47ca3e3852f926a1609b701
OleksyFedorchenko <otihiy@gmail.com>
2023-03-09 17:20:31 +0200

    docker refactoring

--- src/main/java/com/example/lecture18/service/MailSenderServiceProps.java
+++ src/main/java/com/example/lecture18/service/MailSenderServiceProps.java
@@ -1,28 +1,44 @@
 package com.example.lecture18.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 
 
-@Service
+
+@Configuration
+@PropertySource("classpath:.env")
 public class MailSenderServiceProps {
+    @Value(value = "${spring.mail.host}")
+    private String host;
+    @Value(value = "${spring.mail.port}")
+    private int port;
+    @Value(value = "${spring.mail.username}")
+    private String username;
+    @Value(value = "${spring.mail.password}")
+    private String password;
+    @Value(value = "${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+    @Value(value = "${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
 
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost(System.getenv("spring.mail.host"));
-        mailSender.setPort(Integer.parseInt(System.getenv("spring.mail.port")));
+        mailSender.setHost(host);
+        mailSender.setPort(port);
 
-        mailSender.setUsername(System.getenv("spring.mail.username"));
-        mailSender.setPassword(System.getenv("spring.mail.password"));
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", System.getenv("spring.mail.properties.mail.smtp.auth"));
-        props.put("mail.smtp.starttls.enable", System.getenv("spring.mail.properties.mail.smtp.starttls.enable"));
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
         props.put("mail.debug", "false");
 
         return mailSender;
